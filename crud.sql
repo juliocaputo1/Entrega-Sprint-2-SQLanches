1826a6cf516625e75215d091e48ad587de2c59aa
julio <juliocaputo434@gmail.com>
2022-10-03 17:29:40 -0300

    commit-final

--- crud.sql
+++ crud.sql
@@ -1,34 +1,68 @@
 -- Aqui você deve colocar os códigos SQL referentes às
 -- Simulações de um CRUD
-
 -- Criação
-
 -- 1)
-
-
-
--- 2)
-
-
-
--- 3)
-
-
+INSERT INTO
+    clientes (nome, lealdade)
+VALUES
+    ('Geórgia', 0)
+INSERT INTO
+    pedidos (status, cliente_id)
+VALUES
+    ('Recebido', 6)
+INSERT INTO
+    produtos_pedidos (pedido_id, produto_id)
+VALUES
+    (6, 1),
+    (6, 2),
+    (6, 6),
+    (6, 8),
+    (6, 8);
 
 -- Leitura
-
--- 1)
-
-
+-- 2)
+SELECT
+    cl.id,
+    cl.nome,
+    cl.lealdade,
+    pe.id,
+    pe.status,
+    pe.cliente_id,
+    pr.id,
+    pr.nome,
+    pr.tipo,
+    pr."preço",
+    pr.pts_de_lealdade
+FROM
+    clientes cl
+    INNER JOIN pedidos pe ON pe.cliente_id = cl.id
+    INNER JOIN produtos_pedidos pp ON pp.pedido_id = pe.id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id
+WHERE
+    cl.id = 6;
 
 -- Atualização
-
--- 1)
-
-
+-- 3)
+UPDATE
+    clientes
+SET
+    lealdade = (
+        SELECT
+            SUM(pr.pts_de_lealdade) as pts
+        FROM
+            produtos_pedidos pp
+            INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+            INNER JOIN produtos pr ON pr.id = pp.produto_id
+            INNER JOIN clientes cl ON cl.id = pe.cliente_id
+        WHERE
+            pe.cliente_id = 6
+    )
+WHERE
+    nome = 'Geórgia' RETURNING *;
 
 -- Deleção
-
--- 1)
-
-
+-- 4)
+DELETE FROM
+    clientes
+WHERE
+    nome = 'Marcelo' RETURNING *;
--- selects.sql
+++ selects.sql
@@ -1,12 +1,53 @@
 -- Aqui você deve colocar os códigos SQL referentes às
 -- Seleções de dados
-
 -- 1)
+SELECT
+    id
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id;
 
 -- 2)
+SELECT
+    pe.id
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id
+WHERE
+    pr.nome = 'Fritas';
 
 -- 3)
+SELECT
+    cl.nome AS gostam_de_fritas
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id
+    INNER JOIN clientes cl ON cl.id = pe.cliente_id
+WHERE
+    pr.id = 6;
 
 -- 4)
+SELECT
+    SUM(pr.preço)
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id
+    INNER JOIN clientes cl ON cl.id = pe.cliente_id
+WHERE
+    cl.nome = 'Laura';
 
 -- 5)
+SELECT
+    pr.nome,
+    COUNT(pp.produto_id)
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos pe ON pe.id = pp.pedido_id
+    INNER JOIN produtos pr ON pr.id = pp.produto_id
+    INNER JOIN clientes cl ON cl.id = pe.cliente_id
+GROUP BY
+    pr.nome
